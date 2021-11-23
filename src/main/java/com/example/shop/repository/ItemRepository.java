feb011556e42863f2373641a5135cd87ce9e6746
yulhee741 <dbfgml980413@gmail.com>
2021-11-23 21:57:37 +0900

    엔티티 설계

---
+++ src/main/java/com/example/shop/repository/ItemRepository.java
@@ -0,0 +1,11 @@
+package com.example.shop.repository;
+
+import com.example.shop.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByItemNm(String itemNm);
+}
