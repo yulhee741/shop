17bff52d6ae0bb26c5877e37be28ad2f4afe7ac8
yulhee741 <dbfgml980413@gmail.com>
2021-12-09 00:26:23 +0900

    연관관계 매핑하기

---
+++ src/main/java/com/example/shop/entity/OrderItem.java
@@ -0,0 +1,32 @@
+package com.example.shop.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+public class OrderItem {
+
+    @Id @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Order item;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private int orderPrice; //주문 가격
+
+    private int count; //수량
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+}
