8c39cb64a6aa1ed2b398053415a79894dbca2a24
yulhee741 <dbfgml980413@gmail.com>
2021-12-08 22:53:50 +0900

    회원 로그인 처리 및 페이지 권한 설정하기

--- src/main/java/com/example/shop/controller/MemberController.java
+++ src/main/java/com/example/shop/controller/MemberController.java
@@ -23,7 +23,7 @@ public class MemberController {
     private final PasswordEncoder passwordEncoder;
 
     @GetMapping(value = "/new")
-    public String memberForm(Model model) {
+    public String memberForm(Model model){
         model.addAttribute("memberFormDto", new MemberFormDto());
         return "member/memberForm";
     }
@@ -44,6 +44,17 @@ public class MemberController {
         }
 
         return "redirect:/";
+    }
+
+    @GetMapping(value = "/login")
+    public String loginMember(){
+        return "/member/memberLoginForm";
+    }
+
+    @GetMapping(value = "/login/error")
+    public String loginError(Model model){
+        model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요");
+        return "/member/memberLoginForm";
     }
 
 }
