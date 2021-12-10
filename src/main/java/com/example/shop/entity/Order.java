b5b40c6919afc19e62e894c9eeb1d4304c81c292
yulhee741 <dbfgml980413@gmail.com>
2021-12-10 18:53:24 +0900

    영속성 전이, 지연 로딩

--- src/main/java/com/example/shop/entity/Order.java
+++ src/main/java/com/example/shop/entity/Order.java
@@ -19,7 +19,7 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
@@ -28,7 +28,8 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus; //주문상태
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL
+            , orphanRemoval = true, fetch = FetchType.LAZY)
     private List<OrderItem> orderItems = new ArrayList<>();
 
     private LocalDateTime regTime;
