9430826fc7405cf8e58b13fa59666ad5038b413a
yulhee741 <dbfgml980413@gmail.com>
2021-12-13 23:15:38 +0900

    feat: 상품 등록하기

---
+++ src/main/java/com/example/shop/service/ItemService.java
@@ -0,0 +1,49 @@
+package com.example.shop.service;
+
+import com.example.shop.dto.ItemFormDto;
+import com.example.shop.entity.Item;
+import com.example.shop.entity.ItemImg;
+import com.example.shop.repository.ItemImgRepository;
+import com.example.shop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    private final ItemImgService itemImgService;
+
+    private final ItemImgRepository itemImgRepository;
+
+    public Long saveItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) throws Exception{
+
+        //상품 등록
+        Item item = itemFormDto.createItem();
+        itemRepository.save(item);
+
+        //이미지 등록
+        for(int i=0;i<itemImgFileList.size();i++){
+            ItemImg itemImg = new ItemImg();
+            itemImg.setItem(item);
+
+            if(i == 0)
+                itemImg.setRepimgYn("Y");
+            else
+                itemImg.setRepimgYn("N");
+
+            itemImgService.saveItemImg(itemImg, itemImgFileList.get(i));
+        }
+
+        return item.getId();
+    }
+
+}
